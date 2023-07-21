9fe3584ea1302df1558a689a098e40fc0f171eb7
Christian Vaughn <me@christianvaughn.net>
2023-07-20 19:54:40 -0700

    update logo

--- Monkeytype Logo template 2k2k.user.js
+++ Monkeytype Logo template 2k2k.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the canvas!
 // @author       ze_or
-// @match        https://hot-potato.reddit.com/embed*
+// @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].getElementsByTagName("garlic-bread-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/ze-or/r-place-MT/main/Dotted Monkeytype Logo Template 2k2k.png";
+            i.src = "https://raw.githubusercontent.com/ChristianVaughn/r-place/main/Dotted Monkeytype Logo Template 2k2k.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
